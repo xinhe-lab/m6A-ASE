18bd3041213b882d7ccd72632929ec41989a5b30
Min Qiao <qiaomin312@gmail.com>
2018-02-14 17:21:02 -0600

    Build website with jnbinder

---
+++ docs/js/docs.js
@@ -0,0 +1,2 @@
+var analysisArray = ['nonpeak and RBP binding sites**](analysis/20180212_Fisher_2by2_RBP-miRNA_with_m6A_peak-nonpeak']
+var analysisDict = {"Fisher-Exact-test-for-m6A-peak-nonpeak-and-RBP-binding-sites-1":"20180212_Fisher_2by2_RBP-miRNA_with_m6A_peak-nonpeak"}
