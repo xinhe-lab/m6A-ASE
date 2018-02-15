6802016176e6c2970ba4cb013292794e6b5e83db
Min Qiao <qiaomin312@gmail.com>
2018-02-14 19:18:16 -0600

    Build website with jnbinder

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,2 +1,2 @@
-var analysisArray = ['nonpeak and RBP binding sites**](analysis/20180212_Fisher_2by2_RBP-miRNA_with_m6A_peak-nonpeak']
-var analysisDict = {"Fisher-Exact-test-for-m6A-peak-nonpeak-and-RBP-binding-sites-1":"20180212_Fisher_2by2_RBP-miRNA_with_m6A_peak-nonpeak"}
+var analysisArray = ['20180214_Overview_FisherExactTest_m6A_peak-nonpeak_RBP-miRNA', '20180212_Fisher_2by2_RBP-miRNA_with_m6A_peak-nonpeak']
+var analysisDict = {"Fisher-Exact-test-for-m6A-peak-nonpeak-and-RBP-binding-sites-1":"20180212_Fisher_2by2_RBP-miRNA_with_m6A_peak-nonpeak","Overview-Fisher-Exact-test-result-1":"20180214_Overview_FisherExactTest_m6A_peak-nonpeak_RBP-miRNA"}
