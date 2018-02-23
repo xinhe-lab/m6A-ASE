444b1dba267aabfaff284431032cfc1798140157
Min Qiao <qiaomin312@gmail.com>
2018-02-23 17:16:59 -0600

    Build website with jnbinder

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,6 +1,6 @@
 var highlightsArray = []
 var highlightsDict = {"Overview-Fisher-Exact-test-result-1":"20180214_FisherExactTest_m6A_peak-nonpeak_RBP-miRNA"}
 var analysisArray = []
-var analysisDict = {"import-pandas-as-pd-1":"180222_m6A_riboSNitch","Fisher-Exact-test-for-m6A-peak-nonpeak-and-RBP-binding-sites-1":"20180212_Fisher_2by2_RBP-miRNA_with_m6A_peak-nonpeak"}
+var analysisDict = {"Fisher-Exact-test-for-m6A-peak-nonpeak-and-RBP-binding-sites-1":"20180212_Fisher_2by2_RBP-miRNA_with_m6A_peak-nonpeak","import-pandas-as-pd-1":"20180222_m6A_riboSNitch"}
 var writeupArray = []
 var writeupDict = {}
